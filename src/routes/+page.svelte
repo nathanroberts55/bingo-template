d2b0da45b3d5074c796eb9a3636e187af7fd6f09
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-08-15 23:13:06 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Bingo from './Bingo.svelte';
+</script>
+
+<svelte:head>
+	<title>Bingo Template</title>
+</svelte:head>
+
+<div class="flex flex-col mx-auto my-auto">
+	<h1 class="text-2xl text-center py-2">Welcome to Bingo Template</h1>
+
+	<Bingo />
+</div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,7 @@
+<div class="mx-auto my-10">
+	<p class="text-4xl text-center">Contact</p>
+	<p class="text-2xl py-4 text-center">Enter your own Contact Info</p>
+	<a class="text-center" href="https://github.com/nathanroberts55/bingo-template"
+		><p>Create Issue For Any Questions about the Template</p></a
+	>
+</div>
